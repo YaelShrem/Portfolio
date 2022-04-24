03bf6a607b03d4686d5da608a73ed24fb376390f
YaelBusso <y0583239076@gmail.com>
2022-04-25 00:07:19 +0300

    more

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,37 +1,36 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
-import LogoTitle from '../../assets/images/logo-s.png'
-import Logo from './Logo'
+// import AnimatedLetters from '../AnimatedLetters'
+// import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
-  const jobArray = [
-    'w',
-    'e',
-    'b',
-    ' ',
-    'd',
-    'e',
-    'v',
-    'e',
-    'l',
-    'o',
-    'p',
-    'e',
-    'r',
-    '.',
-  ]
+    const nameArray = ['Y', 'a', 'e', 'l', ' ', 'S', 'h', 'r', 'e', 'm']
+    const jobArray = [
+      'w',
+      'e',
+      'b',
+      ' ',
+      'd',
+      'e',
+      'v',
+      'e',
+      'l',
+      'o',
+      'p',
+      'e',
+      'r',
+      '.',
+    ]
 
-  useEffect(() => {
-    return setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-  }, [])
+    // useEffect(() => {
+    //   return setTimeout(() => {
+    //     setLetterClass('text-animate-hover')
+    //   }, 4000)
+    // }, [])
 
   return (
     <>
@@ -43,28 +42,25 @@ const Home = () => {
             <br />
             <span className={`${letterClass} _13`}>I</span>
             <span className={`${letterClass} _14`}>'m</span>
-            <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
-            />
-            <AnimatedLetters
+            <p>Yael Shrem</p>
+            {/* <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
               idx={15}
-            />
+            /> */}
             <br />
-            <AnimatedLetters
+            {/* <AnimatedLetters
               letterClass={letterClass}
               strArray={jobArray}
               idx={22}
-            />
+            /> */}
           </h1>
-          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <h2>Full Stack Developer</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
         </div>
-        <Logo />
+        {/* <Logo /> */}
       </div>
 
       <Loader type="pacman" />
@@ -72,4 +68,4 @@ const Home = () => {
   )
 }
 
-export default Home;
+export default Home
