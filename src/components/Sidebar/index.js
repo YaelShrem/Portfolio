422eda57fc0532b372a37a0356201244a1a6416c
YaelBusso <y0583239076@gmail.com>
2022-04-24 23:07:54 +0300

    navbar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,49 +1,48 @@
 import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
-import LogoYS from '../../assets/images/ys-logo.png'
-import Icon from '../Icon';
+// import LogoYS from '../../assets/images/ys-logo.png'
+import Icon from '../Icon'
 
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
-      <img src={LogoYS} alt="logo" />
-      <h2 className="sub-logo">Yael Shrem</h2>
+      <Icon name="ysLogo"/>
+      {/* <img src={LogoYS} alt="logo" /> */}
+      <h3 className="sub-logo">Yael Shrem</h3>
       {/* <img className="sub-logo" src={LogoSubtitle} alt="Yael Shrem" /> */}
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
-        <Icon name="copy"/>
+        <Icon name="home" />
       </NavLink>
       <NavLink activeclassname="active" className="about-link" to="/about">
-        <Icon name="copy"/>
+        <Icon name="about" />
       </NavLink>
-      <NavLink
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
-        <Icon name="copy"/>
-        </NavLink>
+      <NavLink activeclassname="active" className="skills-link" to="/skills">
+        <Icon name="skills" />
+      </NavLink>
+      <NavLink activeclassname="active" className="projects-link" to="/projects">
+        <Icon name="eye" />
+      </NavLink>
+      <NavLink activeclassname="active" className="contact-link" to="/contact">
+        <Icon name="mail" />
+      </NavLink>
     </nav>
     <ul>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/yaelshrem/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Icon name="copy"/>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/yaelshrem"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <Icon name="copy"/>
-          </a>
-        </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/yaelshrem/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Icon name="linkedinLogo" />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/yaelshrem" target="_blank" rel="noreferrer">
+          <Icon name="githubLogo" />
+        </a>
+      </li>
     </ul>
   </div>
 )
